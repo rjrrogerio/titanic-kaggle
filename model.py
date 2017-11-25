f21dc2f107c203ee6fd2f8123608ef33e381146a
aajjbb <jefersonlsiq@gmail.com>
2017-11-25 15:56:19 -0200

    starting to use gradient boosted decision tree

--- model.py
+++ model.py
@@ -1,34 +1,36 @@
 import pandas as pd
 
-from sklearn.neural_network import MLPClassifier
+# from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingClassifier
+# from sklearn.neural_network import MLPClassifie
 from sklearn.preprocessing import Imputer
 
 if __name__ == "__main__":
     train_data_filename = "train.csv"
     test_data_filename = "test.csv"
 
-    mlp = MLPClassifier()
+    mlp = GradientBoostingClassifier()
     my_imputer = Imputer()
 
-    predictors = ['Pclass', 'Age', 'SibSp',
-                  'Parch', 'Fare']
+    predictors = ['Pclass', 'Sex', 'Age', 'SibSp',
+                  'Parch', 'Fare', 'Embarked']
 
     train_data = pd.read_csv(train_data_filename)
     test_data = pd.read_csv(test_data_filename)
 
-    train_X = pd.get_dummies(train_data[predictors])
+    train_X = pd.get_dummies(pd.get_dummies(train_data[predictors]))
     train_y = train_data.Survived
 
-    test_X = test_data[predictors]
+    test_X = pd.get_dummies(test_data[predictors])
 
     # imputed inputs
     # NaN are replaced with real numbers
-    imputed_train_X = my_imputer.fit_transform(train_X)
-    imputed_test_X = my_imputer.transform(test_X)
+    train_X = my_imputer.fit_transform(train_X)
+    test_X = my_imputer.transform(test_X)
 
-    mlp.fit(imputed_train_X, train_y)
+    mlp.fit(train_X, train_y)
 
-    y = mlp.predict(imputed_test_X)
+    y = mlp.predict(test_X)
 
     output_data = {
         'PassengerId': test_data.PassengerId,
